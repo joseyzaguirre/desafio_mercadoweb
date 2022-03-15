2a6f722bcf7a30f6a63c5c2123ae2732a323dfd2
José Eyzaguirre <josemanuel.eyzaguirre@gmail.com>
2022-03-15 16:49:34 -0300

    pequeño avance

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 
 const app = express()
 app.use(express.static('static'))
+app.use(express.static('node_modules/bootstrap/dist'))
+
+nunjucks.configure('views', {
+    express: app,
+    autoescape: true
+})
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
 
 app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'))
