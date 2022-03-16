7a3c70473a3471273f5a71541ce2b246a848a9bd
José Eyzaguirre <josemanuel.eyzaguirre@gmail.com>
2022-03-15 23:02:47 -0300

    avance

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const fs = require('fs').promises
 
 const app = express()
 app.use(express.static('static'))
@@ -10,8 +11,14 @@ nunjucks.configure('views', {
     autoescape: true
 })
 
-app.get('/', (req, res) => {
-    res.render('index.html')
+app.get('/', async (req, res) => {
+
+    let data = await fs.readFile("productos.json", 'utf-8');
+    data = JSON.parse(data)
+    const productos = data.productos
+    console.log({productos})
+
+    res.render('index.html', { productos })
 })
 
 app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'))
